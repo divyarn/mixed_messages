671b9c9b650067849e2a0d792881f33cd37696da
Divya Nandihalli <divyarn29@gmail.com>
2021-03-17 17:52:15 -0700

    First bringup of random quote

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 console.log('Your inspirational message of the day is - ');
+
+const inspirationalQuotes = [ 
+    'Every Dog has its day', 
+    'Never Stop being a good person because of bad people'
+                        ];
+
+let randIdx = Math.floor(Math.random()*inspirationalQuotes.length);
+
+console.log(inspirationalQuotes[randIdx]);
